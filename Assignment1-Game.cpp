5ee44a60543b7005d8ff11f3814145f565e1500f
Myeongseop Kim <power2365@gmail.com>
2021-04-19 01:12:02 +0900

    <고양이와 놀기> 게임 완료한 커밋입니다.

--- Assignment1-Game.cpp
+++ Assignment1-Game.cpp
@@ -7,20 +7,20 @@
 
 SceneID scene1, scene2, scene3;
 SoundID sound1;
-ObjectID button1, button2, marble_numberbutton[4], backbutton[2], startbutton[2], endbutton[3], mixbutton;
+ObjectID button1, button2, marble_numberbutton[4], backbutton[2], startbutton[2], endbutton[3];
+ObjectID easybutton, normalbutton, hardbutton, hellbutton;
 ObjectID papercup[4], marble, arrow1, arrow2;
 int cupX[4] = { 200,450,680,900 };
 int marbleX[4] = { 270,520,750,975 };
 int marbleY = 500;
 
 TimerID game1_timer1, game1_timer2, game1_timer3;
-float timerValue1 = 0.01f, timerValue2 = 0.5f, timerValue3 = 0.5f;
+float timerValue1 = 0.01f, timerValue2, timerValue3;
 
 int index1;		//사용자가 몇번째에 구슬을 놓을건지 결정, 그리고 그 값 저장
-int answer;
+int index2;
 
-int mixCount = 5;	//컵 섞는 횟수
-int mixCount_origin = 5;
+int mixCount, mixCount_origin;	//컵 섞는 횟수
 
 //음악재생 함수
 SoundID playsound(SoundID sound, const char* soundname, const char* soundfile, bool playing , bool loop) {
@@ -77,9 +77,11 @@ void marble_drop(int i) {
 			papercup[i] = DIY(papercup[i], "컵", "Images/papercup.png", scene2, cupX[i], 30, true);
 			scaleObject(papercup[i], 1.6f);
 		}
-		mixbutton = DIY(mixbutton, "섞기버튼", "Images/mix.png", scene2, 620, 380);
-		scaleObject(mixbutton, 1.2f);
-		showMessage("화살표가 뜨는 컵끼리 섞는 것입니다.");
+		easybutton = DIY(easybutton, "난이도 쉬움", "Images/easy.png", scene2, 120, 460);
+		normalbutton = DIY(normalbutton, "난이도 보통", "Images/normal.png", scene2, 420, 460);
+		hardbutton = DIY(hardbutton, "난이도 어려움", "Images/hard.png", scene2, 780, 460);
+		hellbutton = DIY(hellbutton, "난이도 지옥", "Images/hell.png", scene2, 1100, 460);
+		showMessage("난이도를 선택해주세요.\n참고로 화살표는 섞는 컵을 알려줍니다.");
 	}
 }
 
@@ -103,12 +105,16 @@ void mix_cup() {
 		arrow1 = DIY(arrow1, "화살표", "Images/arrow.png", scene2, cupX[a] + 70, 332);
 		arrow2 = DIY(arrow2, "화살표", "Images/arrow.png", scene2, cupX[b] + 70, 332);
 
-		if (a == index1) index1 = a;
-		else if (b == index1) index1 = b;
+		if (a == index2) index2 = b;
+		else if (b == index2) index2 = a;
 		mixCount--;
 
 		setTimer(game1_timer3, timerValue3);
 		startTimer(game1_timer3);
+	}
+	else {
+		setTimer(game1_timer2, timerValue2);
+		startTimer(game1_timer2);
 	}
 }
 
@@ -148,33 +154,56 @@ void mousecallback(ObjectID object, int x, int y, MouseAction action) {
 			locateObject(marble, scene2, marbleX[0], 500);
 			hideObject(marble);
 			marbleY = 500;
-			hideObject(mixbutton);
 			hideObject(arrow1);
 			hideObject(arrow2);
 			mixCount = mixCount_origin;
+			hideObject(easybutton); hideObject(normalbutton); hideObject(hardbutton); hideObject(hellbutton);
+			stopTimer(game1_timer2);	stopTimer(game1_timer3);
 		}
 		else if (object == marble_numberbutton[index1]) {
 			marble = DIY(marble, "구슬", "Images/earthmarble.png", scene2, marbleX[index1], marbleY, true);
 			marble_drop(index1);
+			index2 = index1;
 			for (int i = 0; i < 4; i++) hideObject(marble_numberbutton[i]);
 		}
-		else if (object == mixbutton) {
+		else if (object == easybutton) {
+			timerValue2 = 0.7f, timerValue3 = 0.7f;
+			mixCount = 3, mixCount_origin = 3;
 			mix_cup();
-			hideObject(mixbutton);
+			hideObject(easybutton); hideObject(normalbutton); hideObject(hardbutton); hideObject(hellbutton);
+		}
+		else if (object == normalbutton) {
+			timerValue2 = 0.52f, timerValue3 = 0.52f;
+			mixCount = 8, mixCount_origin = 8;
+			mix_cup();
+			hideObject(easybutton); hideObject(normalbutton); hideObject(hardbutton); hideObject(hellbutton);
+		}
+		else if (object == hardbutton) {
+			timerValue2 = 0.3f, timerValue3 = 0.3f;
+			mixCount = 18, mixCount_origin = 18;
+			mix_cup();
+			hideObject(easybutton); hideObject(normalbutton); hideObject(hardbutton); hideObject(hellbutton);
+		}
+		else if (object == hellbutton) {
+			timerValue2 = 0.18f, timerValue3 = 0.18f;
+			mixCount = 15, mixCount_origin = 15;
+			mix_cup();
+			hideObject(easybutton); hideObject(normalbutton); hideObject(hardbutton); hideObject(hellbutton);
 		}
 		else if (game1_end == true) {
-			if (object == papercup[index1]) {
+			if (object == papercup[index2]) {
 				showMessage("정답입니다!!");
 				for (int i = 0; i < 4; i++) hideObject(papercup[i]);
 				game1_end = false;
 			}
-			else if (object != papercup[index1]) {
+			else if (object != papercup[index2]) {
 				showMessage("틀렸습니다...");
 				for (int i = 0; i < 4; i++) hideObject(papercup[i]);
 				game1_end = false;
 			}
 		}
 	}
+
 	else if (game1_on == false && game2_on == true) {
 		if (object == backbutton[1]) {
 			enterScene(scene3);
@@ -195,10 +224,9 @@ void timercallback(TimerID timer) {
 		}
 		else if (mixCount <= 0) {
 			showMessage("다 섞었습니다!\n정답을 골라주세요.");
-			answer = index1;
 			hideObject(arrow1);
 			hideObject(arrow2);
-			locateObject(marble, scene2, marbleX[answer], marbleY);
+			locateObject(marble, scene2, marbleX[index2], marbleY);
 			game1_end = true;
 		}
 	}
@@ -245,10 +273,6 @@ int main() {
 	endbutton[0] = DIY(endbutton[0], "종료", "Images/end.png", scene1, 1150, 300);
 	endbutton[1] = DIY(endbutton[1], "종료", "Images/end.png", scene2, 1150, 300);
 	endbutton[2] = DIY(endbutton[2], "종료", "Images/end.png", scene3, 490, 60);
-
-	//야바위 구슬 배치
-	//marble = DIY(marble, "구슬", "Images/earthmarble.png", scene2, marbleX[i], marbleY);
-
 
 	marble_numberbutton[0] = DIY(marble_numberbutton[0], "숫자1", "Images/number-1.png", scene2, 270, 285, false);
 	marble_numberbutton[1] = DIY(marble_numberbutton[1], "숫자2", "Images/number-2.png", scene2, 520, 285, false);
